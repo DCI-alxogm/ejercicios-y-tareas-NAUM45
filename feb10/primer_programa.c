0284902f4ab779a13c49ffcb657a1f792cc9cdd7
Naum45 <un.ibarrapere@ugto.mx>
2025-02-10 11:31:22 -0600

    Guarde: primer_programa.c

---
+++ feb10/primer_programa.c
@@ -0,0 +1,8 @@
+/*Creado el 10 de febrero del 2025 por NAUM IBARRA*/
+#include<stdio.h>
+int main()
+{
+printf("Hola NAUM \n");
+printf("Este es el primer programa del curso\n");
+}
+/*Termine de escribir el programa*/
