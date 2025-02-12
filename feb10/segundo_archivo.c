ed2b0d766a020752f6555c3ec1e18a0bf970c5fa
Naum45 <un.ibarrapere@ugto.mx>
2025-02-12 10:08:31 -0600

    hice el segundo archivo

---
+++ feb10/segundo_archivo.c
@@ -0,0 +1,8 @@
+/* este es mi segundo archivo NAUM*/
+#include<stdio.h>
+int main()
+{
+printf("Hola NAUM \n");
+printf("Este es el segundo programa del curso\n");
+}
+/*Termine de escribir el programa 
