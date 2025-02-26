e5d91a146a567602813887622e9e2b2d09022b59
Naum45 <un.ibarrapere@ugto.mx>
2025-02-26 11:47:54 -0600

    hice la primera y segunda parte de ejemplos

---
+++ Feb26/Par_inpar.c
@@ -0,0 +1,27 @@
+/*
+HOLA soy naum ibarra este es el trabajo de clase del dia 26 de febrero del 2025*/
+
+//Determinar si un nombre es par o impar
+#include <stdio.h>
+
+int main(){
+char si,no;
+    int numero,residuo;
+    printf("Que numero quieres saber si es par o inpar\n");
+    scanf("%i", &numero);
+    residuo=numero%2;
+    if(residuo==0){
+    printf("%i es par\n",numero);
+    }else{
+    
+    
+     printf("%i es impar\n",numero);
+     }
+    return(0);
+    printf("¿quieres saber si otro numero es par o impar?(si,no);
+    scanf("%i"respuesta);
+   
+  
+    
+    }
+
