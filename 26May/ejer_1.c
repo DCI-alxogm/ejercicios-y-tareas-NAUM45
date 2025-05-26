be364292326ecdcb1d14d88c566273a0d8503f52
root <root@HPNAUM.>
2025-05-26 10:58:19 -0600

    ES el ejercicio de clase

---
+++ 26May/ejer_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+void cuadrado(); int main(){
+ 
+cuadrado();
+ 
+return (0);
+ 
+void cuadrado(){
+ 
+X
+ 
+float x, x2; printf("Introduce un numero\n"); scanf("%f", &x);
+ 
+printf("El cuadrado de %f es %f\n", x, x2);
+
