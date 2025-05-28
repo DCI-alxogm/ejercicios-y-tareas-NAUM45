d147bf0eac9b8a9fa839b773d6b782a0ea999399
root <root@HPNAUM.>
2025-05-28 10:56:31 -0600

    Ejercicio 4

---
+++ 26May/fun_void_entrada.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void cuadrado (float x);
+
+int main(){
+float x;
+printf("Introduce un numero");
+scanf("f", &x);
+cuadrado(x);
+return (0);
+}
+void cuadrado (float x){
+float x2;
+x2=_x*_x;
+printf("El cuadrdo de *f es *f \n", x,x2);
+}
